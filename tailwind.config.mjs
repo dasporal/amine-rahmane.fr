74105082b49f2c31e68ff329af44476a696134f7
dasporal <dasporal@gmail.com>
2023-11-02 16:55:05 +0100

    general layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+        'text': '#f0dddb',
+        'background': '#06000a',
+        'primary': '#9f2b1e',
+        'secondary': '#0c1b1d',
+        'accent': '#4770ae',
+       },
+       
+
+		},
 	},
 	plugins: [],
 }
