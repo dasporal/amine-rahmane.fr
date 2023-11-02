fbdeaa751c23e97ac0639dc3c949a987d9af4df6
dasporal <dasporal@gmail.com>
2023-11-02 18:04:19 +0100

    background image for main card

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
         'text': '#f0dddb',
         'background': '#06000a',
         'primary': '#9f2b1e',
         'secondary': '#0c1b1d',
         'accent': '#4770ae',
-       },
-       
-
-		},
-	},
-	plugins: [],
+      },
+      fontFamily: {
+        title: 'Fira Sans, sans-serif',
+        body: 'IBM Plex Sans, sans-serif',
+      },
+    },
+  },
+  plugins: [],
 }
