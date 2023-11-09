2636c051833df35d59385eecdc2b74ba5a5a99af
dasporal <dasporal@gmail.com>
2023-11-09 09:39:07 +0100

    fixed import DecapCMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import NetlifyCMS from 'astro-netlify-cms';
 
 // https://astro.build/config
 export default defineConfig({
