b5113957b79543fe56cdf62848d2430b641d767b
dasporal <dasporal@gmail.com>
2023-11-09 09:35:54 +0100

    decapcms integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,34 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(),
+  NetlifyCMS({
+    config: {
+      locale: 'fr',
+      backend: {
+        name: 'git-gateway',
+        repo: 'dasporal/amine-rahmane',
+        branch: 'main',
+      },
+      local_backend: true,
+      site_url: 'https://amine-rahmane.fr/',
+      display_url: 'https://amine-rahmane.fr/',
+      media_folder: 'static/assets',
+      collections: [
+        {
+          name: 'works',
+          label: 'Works',
+          folder: 'static/works',
+          create: true,
+          delete: true,
+          fields: [
+            { name: 'templateKey', label: 'Template Key', required: true, widget: 'hidden', default: 'work' },
+            { name: 'project', label: 'Project' },
+            { name: 'company', label: 'Company' },
+            { name: 'context', label: 'Context', widget: 'text' }
+          ],
+        },
+      ],
+    },
+  }),]
 });
