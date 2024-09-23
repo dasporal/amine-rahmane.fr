01f0229c0b3e86f5c75fcbe91c4ff6616c96b693
dasporal <dasporal@gmail.com>
2024-09-23 17:01:44 +0200

    trying aceternity

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,8 @@
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class'],
@@ -68,5 +73,17 @@ export default {
       'ibm-plex-sans': ["'IBM Plex Sans', sans-serif"],
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate'), addVariablesForColors],
+}
+
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
 }
