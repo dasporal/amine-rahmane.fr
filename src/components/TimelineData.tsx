6f94d9faca867ad3f02c4b6efa39ba604af1a97f
dasporal <dasporal@gmail.com>
2024-09-25 23:34:02 +0200

    feat(projects): added metapulse

--- src/components/TimelineData.tsx
+++ src/components/TimelineData.tsx
@@ -1,31 +1,37 @@
-import { Timeline } from "@/components/ui/timeline";
+import { Timeline } from '@/components/ui/timeline'
 
-import reciple from "@/assets/projects/reciple/hero.png"
-import workos from "@/assets/projects/authkit/workos.svg"
-import efrei from "@/assets/projects/efrei/hero.png"
-import santevet from "@/assets/projects/santevet/hero.png"
-import remarqr from "@/assets/projects/remarqr/hero.png"
-import shortedition from "@/assets/projects/short-edition/logo.svg"
-import teksial from "@/assets/projects/teksial/logo.png"
-import bacchus from "@/assets/projects/bacchus/hero.png"
-import petitponey from "@/assets/projects/petit-poney/hero.png"
-import thronocrigger from "@/assets/projects/thronocrigger/hero.png"
-import samuel from "@/assets/projects/samuel-hebting/hero.png"
+import reciple from '@/assets/projects/reciple/hero.png'
+import workos from '@/assets/projects/authkit/workos.svg'
+import efrei from '@/assets/projects/efrei/hero.png'
+import santevet from '@/assets/projects/santevet/hero.png'
+import remarqr from '@/assets/projects/remarqr/hero.png'
+import shortedition from '@/assets/projects/short-edition/logo.svg'
+import teksial from '@/assets/projects/teksial/logo.png'
+import amsecurite from '@/assets/projects/am-securite/hero.png'
+import bacchus from '@/assets/projects/bacchus/hero.png'
+import petitponey from '@/assets/projects/petit-poney/hero.png'
+import thronocrigger from '@/assets/projects/thronocrigger/hero.png'
+import samuel from '@/assets/projects/samuel-hebting/hero.png'
 
 export default function TimelineData() {
   const data = [
     {
-      title: "2024",
+      title: '2024',
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            Chef de projet en freelance chez ORPI. Mise en place de processus de gestion de projet incluant Jira, Confluence et Gitlab, tout en formant les équipes.
+            Chef de projet en freelance chez ORPI. Mise en place de processus de
+            gestion de projet incluant Jira, Confluence et Gitlab, tout en
+            formant les équipes.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            Création de Reciple, un jeu similaire à Wordle centré sur la nourriture, avec Svelte.
+            Création de Reciple, un jeu similaire à Wordle centré sur la
+            nourriture, avec Svelte.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-          Développement de Remarqr, une solution SaaS conçue pour simplifier la gestion des annotations et améliorer la collaboration entre développeurs et parties prenantes métier.
+            Développement de Remarqr, une solution SaaS conçue pour simplifier
+            la gestion des annotations et améliorer la collaboration entre
+            développeurs et parties prenantes métier.
           </p>
           <div className="grid grid-cols-2 gap-4">
             <img
@@ -47,14 +53,17 @@ export default function TimelineData() {
       ),
     },
     {
-      title: "2023",
+      title: '2023',
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            Développeur Web Fullstack freelance chez Santevet. Contributions au développement de l'ERP et création de nouveaux microservices en Python et Go sur AWS.
+            Développeur Web Fullstack freelance chez Santevet. Contributions au
+            développement de l'ERP et création de nouveaux microservices en
+            Python et Go sur AWS.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            Modernisation d'un projet PHP legacy pour AdopteUnePiece et création de plusieurs sites pour divers clients.
+            Modernisation d'un projet PHP legacy pour AdopteUnePiece et création
+            de plusieurs sites pour divers clients.
           </p>
           <div className="grid grid-cols-2 gap-4">
             <img
@@ -90,14 +99,17 @@ export default function TimelineData() {
       ),
     },
     {
-      title: "2022",
+      title: '2022',
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            Développeur Web Fullstack chez TEKSIAL. Amélioration des workflows en développant des applications Symfony et React.
+            Développeur Web Fullstack chez TEKSIAL. Amélioration des workflows
+            en développant des applications Symfony et React.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            Chef de projet pour le développement d'un Career-Center pour l'EFREI chez Scalian, permettant aux étudiants d'accéder à des ressources pour préparer leur futur professionnel.
+            Chef de projet pour le développement d'un Career-Center pour l'EFREI
+            chez Scalian, permettant aux étudiants d'accéder à des ressources
+            pour préparer leur futur professionnel.
           </p>
           <div className="grid grid-cols-2 gap-4">
             <img
@@ -115,6 +127,13 @@ export default function TimelineData() {
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow"
             />
             <img
+              src={amsecurite.src}
+              alt="Teksial - Amélioration des workflows avec Symfony et React"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow"
+            />
+            <img
               src={teksial.src}
               alt="Teksial - Amélioration des workflows avec Symfony et React"
               width={500}
@@ -126,14 +145,17 @@ export default function TimelineData() {
       ),
     },
     {
-      title: "2020",
+      title: '2020',
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            Développeur Web Fullstack chez Short-Edition. Développement de produits communautaires et gestion de l'architecture système avec AWS et Terraform.
+            Développeur Web Fullstack chez Short-Edition. Développement de
+            produits communautaires et gestion de l'architecture système avec
+            AWS et Terraform.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            Développement de projets pour Synexie, y compris le site du festival Bacchus et l'intranet de Saint-Tropez.
+            Développement de projets pour Synexie, y compris le site du festival
+            Bacchus et l'intranet de Saint-Tropez.
           </p>
           <div className="grid grid-cols-2 gap-4">
             <img
@@ -154,11 +176,11 @@ export default function TimelineData() {
         </div>
       ),
     },
-  ];
-  
+  ]
+
   return (
     <div className="w-full">
       <Timeline data={data} />
     </div>
-  );
+  )
 }
